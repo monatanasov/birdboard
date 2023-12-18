ddc2998778783965fdf84a720528a7729a5a7c20
monatanasov <simeon.g.atanasov@gmail.com>
2023-12-18 10:45:54 +0200

    e-13-task UI updates--no-css

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -9,8 +9,17 @@
     <div>{{ $project->description }}</div>
 
     @foreach($project->tasks as $task)
-        <div class="card mb-3">{{ $task->body }}</div>
+        <div class="card mb-3">
+            <input name="body" value="{{ $task->body }}">
+        </div>
     @endforeach
+        <div class="card mb-3">
+            <form method="POST" action="{{ $project->path() . '/tasks' }}">
+                @csrf
+
+                <input placeholder="Add a new task..." class="w-full" name="body">
+            </form>
+        </div>
 
 </body>
 </html>
--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -15,6 +15,7 @@ class ProjectTasksTest extends TestCase
 
     public function test_only_the_owner_of_a_project_may_add_tasks()
     {
+
         $this->signIn();
 
         $project = Project::factory()->create();
@@ -27,15 +28,37 @@ class ProjectTasksTest extends TestCase
 
     public function test_project_can_have_tasks()
     {
+//        $this->withoutExceptionHandling();
 
         $this->signIn();
 
         $project = Project::factory()->create(['owner_id' => auth()->id()]);
 
-        $this->post($project->path() . '/tasks', ['body' => 'test Task']);
+        $task = $this->post($project->path() . '/tasks', ['body' => 'test Task']);
 
         $this->get($project->path())
             ->assertSee('test Task');
+    }
+
+    public function test_task_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        $project = Project::factory()->create(['owner_id' => auth()->id()]);
+
+        $task = $project->addTask('test Task');
+
+        $this->patch($project->path() . '/tasks/' . $task->id, [
+            'body' => 'changed',
+            'completed' => true
+        ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'body' => 'changed',
+            'completed' => true
+        ]);
     }
 
     public function test_task_requires_a_body()
